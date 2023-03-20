f5d2f6b944a4dc4c9cb5f02586b9422c501605d8
ekaterinarogova <ekatersergeev@gmail.com>
2023-03-20 17:42:46 +0300

    wraped main code in function

--- utils/main.py
+++ utils/main.py
@@ -1,12 +1,16 @@
 from funcs import load_data, sort_data, pick_latest
-from class_ import Operation
+from class_operate import Operation
 
+def main():
+    file = 'operations.json' #исходный файл json с данными
+    sorted_data = sort_data(load_data(file))
+    latest_operations = pick_latest(sorted_data)
 
-file = 'operations.json' #исходный файл json с данными
-sorted_data = sort_data(load_data(file))
-latest_operations = pick_latest(sorted_data)
-
-for i in latest_operations:
+    for i in latest_operations:
     #цикл перебора по списку последних выполненных операций с созданием экземпляров класса Operation
-    example = Operation(i)
-    print(example.display_info())
+        example = Operation(i)
+        print(example.display_info())
+
+
+if __name__ == '__main__':
+    main()
