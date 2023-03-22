6bd22a8baeeaa7f7e406814339a5ab3e22a04ab8
ekaterinarogova <ekatersergeev@gmail.com>
2023-03-22 18:25:20 +0300

    fixed import of json file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from funcs import load_data, sort_data, pick_latest
-from class_operate import Operation
+from utils.class_operate import Operation
+from utils.funcs import load_data, sort_data, pick_latest
+import os.path
+
 
 def main():
-    file = 'utils/operations.json'  # исходный файл json с данными
+    file = os.path.abspath('operations.json')
     sorted_data = sort_data(load_data(file))
     latest_operations = pick_latest(sorted_data)
 
     for i in latest_operations:
-    #цикл перебора по списку последних выполненных операций с созданием экземпляров класса Operation
+        # цикл перебора по списку последних выполненных операций с созданием экземпляров класса Operation
         example = Operation(i)
         print(example.display_info())
 
